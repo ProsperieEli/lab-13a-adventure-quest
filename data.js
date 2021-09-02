ce6a70182650704fdb0d36ba818b2df55f518fd9
Estefani <stef45689@gmail.com>
2021-09-02 18:00:31 -0500

    File Structure and First Page Layout

---
+++ data.js
@@ -0,0 +1,15 @@
+/*
+ where to live: cold -5, hot 10, moderate 5 
+
+ Two values to track over three questions, related to mythological creatures. 
+ - Dragon 
+ - Unicorn 
+ - Nessy 
+
+ Attributes: shyness & dangerous 
+ -after select 1, kind of shy, very dangerous = dragon etc 
+*/
+const mythical = {
+    id: 'dragon', 
+
+}
