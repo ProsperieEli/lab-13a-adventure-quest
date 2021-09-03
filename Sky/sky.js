2692f0a17dd91de0e091f13898c1ec749aadedb3
ProsperieEli <ElijahProsperie@gmail.com>
2021-09-03 19:39:33 -0400

    Made Local storage dynamic on page two

--- Sky/sky.js
+++ Sky/sky.js
@@ -1,2 +1,9 @@
 //const skyData = new URLSearchParams(window.location.search);
+import { getUserData } from "../local-storage-utils.js";
 
+const userInfo = document.getElementById('display-user');
+
+const userData = getUserData();
+
+userInfo.textContent = `Name: ${userData.name}, ${userData.animal}`;
+//shyness > 0 && userData.shyness
