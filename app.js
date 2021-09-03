0a05781a264af753df36a00f6de87036545d9795
Karl Sigel <heykarl@gmail.com>
2021-09-02 17:54:06 -0700

    created all html pages. started app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // import functions and grab DOM elements
+const ourForm = document.querySelector('our-form');
 
 // initialize global state
 
@@ -6,3 +7,9 @@
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
+ourForm.addEventListener('submit',(e) => {
+e.preventDefault();
+
+}
+  // const player = document.querySelector('input:checked');
