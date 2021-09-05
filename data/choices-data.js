52819d2016d861dfe54d11507fa429a008f32512
Karl Sigel <heykarl@gmail.com>
2021-09-05 13:28:33 -0700

    changed a lot of formatting in constellations css because of new font choice. Added new background image for cassiopia. Also edited choices-data. Lastly added min-height to results css.

--- data/choices-data.js
+++ data/choices-data.js
@@ -1,8 +1,8 @@
 const cassiopeia = {
     id: 'cassiopeia',
     title: 'Was there ever any doubt?',
-    img: 'cassiopeia_lg.jpg',
-    description: 'You are out for your daily swim and come across the Nereids.\nYou stop to flirt with the stunning sea nymphs. Just then Cassiopeia\nshows up and starts an argument with the Nereids about who\nis the most beautiful. Cassiopeia claims that she has no equal.\nThey look to you to settle the argument.',
+    img: 'stars-6.jpg',
+    description: 'You are out for your daily swim and come across the Nereids. You stop to flirt with the stunning sea nymphs. Just as you are about to get their number, Cassiopeia shows up and starts an argument with the Nereids about who is the most beautiful. Cassiopeia claims that she has no equal. They look to you to settle the argument.',
     audio: 'https://upload.wikimedia.org/wikipedia/commons/1/1f/Waves.ogg',
     choices: [{
         id: 'constellation-choice1',
@@ -29,7 +29,7 @@ const ursaMajor = {
     id: 'ursaMajor',
     title: 'Your fears laid bear',
     img: 'ursa_major_lg.jpg',
-    description: 'You are out in the woods and come across a giant scary bear.\nBefore you can decide to do anything dumb, the bear speaks.\n"I am Callisto, changed into a bear by the jealous Hera."\nThe bear then pleads with you to help with this awkward situation.',
+    description: 'You are out in the woods and come across a giant scary bear. Before you decide to do something dumb, the bear speaks. "Traveler, do not fear. I am Callisto, changed into a bear by the jealous Hera." The bear then pleads with you to help with this awkward situation.',
     audio: 'https://upload.wikimedia.org/wikipedia/commons/2/20/Walking_through_woods.ogg',
     choices: [{
         id: 'constellation-choice1',
@@ -39,7 +39,7 @@ const ursaMajor = {
     },
     {
         id: 'constellation-choice2',
-        description: 'You ask Hera to turn you into a bear and go live out your days in the forest with Callisto-bear. ',
+        description: 'You ask Hera to turn you into a bear so you can live out your days in the forest with Callisto-bear. ',
         shyness: 10,
         danger: -10
     },
@@ -56,7 +56,7 @@ const virgo = {
     id: 'virgo',
     title: 'Fruit of the Doom',
     img: 'virgo_lg.jpg',
-    description: `It’s spring time and you are walking through the fields. All the plants\nare starting to wither and it’s over 110˚. You come across a stump\nwith an apple and a pear sitting on top. Under the pear is a note:\n“Eat the pear and all will be well."\nThe apple also has a note:\n“Eat the apple and save the day. Also, the pear is poison.”`,
+    description: `It’s midsummer and you are talking a stroll through some fields near your hut. The plants are starting to wither and the sky starts filling with smoke. You come across a stump with an apple and a pear sitting on top. Under the pear is a note: “Eat the pear and all will be well." The apple also has a note: “Eat the apple and save the day. Also, the pear is poison.”`,
     audio: 'https://upload.wikimedia.org/wikipedia/commons/f/fe/Nature_sounds_ambience_in_a_Dordogne_pond.ogg',
     choices: [{
         id: 'constellation-choice1',
@@ -72,7 +72,7 @@ const virgo = {
     },
     {
         id: 'constellation-choice3',
-        description: 'You keep walking because you don’t eat random fruits and you like hot weather.',
+        description: 'You keep walking because you don’t eat random fruits.',
         shyness: 10,
         danger: -10,
     }]
