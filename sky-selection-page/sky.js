8a87ee6882a56c7be17b89208094a411e4aa9a91
ProsperieEli <ElijahProsperie@gmail.com>
2021-09-06 20:58:36 -0400

    Work in progress on HREF

--- sky-selection-page/sky.js
+++ sky-selection-page/sky.js
@@ -1,14 +1,39 @@
 // import { getUserData } from '../local-storage-utils.js';
 import { answeredQuestions } from './answered-all.js';
 import { renderUserHeader } from '../header-input.js';
+import { getUserData } from '../local-storage-utils.js';
+import choices from '../data/choices-data.js';
+
 
 renderUserHeader();
+
+// Setting a declaration for the visited pages
+
 
 const finished = answeredQuestions();
 if (finished) {
     window.location = '../results-page/index.html';
 }
 
+
+const userViewing = getUserData();
+
+const userLinks = document.getElementsByTagName('A')
+// console.log(userLinks[0].innerText);
+
+// const linkId = userLinks[].innerText;
+
+
+for (let view of choices) {
+    if (userViewing.visited[view.id]) {
+        // userLinks.innerText === (alert('You cannot enter again.'));
+        userLinks.innerText;
+        console.log(userLinks.innerText);
+    }
+   
+    
+}
+  
 // document.getElementById('space-audio').volume = 0.1;
 
 // const userInfo = document.getElementById('display-user');
