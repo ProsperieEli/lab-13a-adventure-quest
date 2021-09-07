d8be347667627d7527a4ef409b2689ec95eab996
Karl Sigel <heykarl@gmail.com>
2021-09-06 22:00:42 -0700

    added comment-driven instructions for setting links to inactive after user answers questions on said pages

---
+++ constellations-page/disable-links.js
@@ -0,0 +1,12 @@
+import { getUserData } from '../local-storage-utils.js';
+
+// grab each link in order and run through array
+
+// grab userdata
+
+// for each link, read search value
+
+// set link to active (do nothing?) 
+//if there is no mattching value that = true in userdata.visited
+
+// else, set each a to some other value
